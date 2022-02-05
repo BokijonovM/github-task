6ba78fff7af7f4c8efc17bf43700e6b09d8bda70
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-02-05 07:18:01 +0100

    styled and updated

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import MyNavbar from "./components/MyNavbar";
 import MyFooter from "./components/MyFooter";
 import MyMain from "./components/MyMain";
 import MyHeader from "./components/MyHeader";
+import LeftInfo from "./components/LeftInfo";
+import { Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <MyNavbar />
       <MyHeader />
+
+      <Row>
+        <Col md={4}>
+          <LeftInfo />
+        </Col>
+        <Col md={8}></Col>
+      </Row>
+
       <MyMain />
       <MyFooter />
     </div>
--- src/components/LeftInfo.jsx
+++ src/components/LeftInfo.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import logoUser from "./assets/user.jpg";
+import { Button } from "react-bootstrap";
 
 function LeftInfo() {
-  return <div></div>;
+  return (
+    <div className="leftInfo-main-div">
+      <div>
+        <img
+          className="left-sidebar-user-image"
+          src={logoUser}
+          alt="user-img"
+        />
+        <div className="user-info-container mt-3">
+          <h3 className="text-light mb-n1">Name Surname</h3>
+          <h4 className="text-light mb-0">Username</h4>
+          <h6 className="text-light mb-0 my-3">User position</h6>
+          <Button className="" variant="dark">
+            Edit profile
+          </Button>
+          <p className="py-2 text-light">
+            <i class="bi bi-people-fill"></i> 8 Followers
+            <i class="bi bi-dot"></i>8 Following
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LeftInfo;
